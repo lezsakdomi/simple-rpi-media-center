ff2cb3db316bdb3368cd71f69adfb927e66ac01c
Domonkos Lezsák <lezsakdomi1@gmail.com>
2020-05-22 19:01:40 +0200

    Homepage

--- app/components/Home.tsx
+++ app/components/Home.tsx
@@ -6,8 +6,30 @@ import styles from './Home.css';
 export default function Home() {
   return (
     <div className={styles.container} data-tid="container">
-      <h2>Home</h2>
-      <Link to={routes.COUNTER}>to Counter</Link>
+      <MenuItem to={routes.HDMI_PLAYER} icon="tv">
+        TV
+      </MenuItem>
+      <MenuItem to={routes.LCD_PLAYER} icon="tablet">
+        képernyő
+      </MenuItem>
+      <MenuItem to="#" icon="th">
+        kiválasztás
+      </MenuItem>
+      <MenuItem to={routes.SERVICE} icon="tools">
+        szervíz
+      </MenuItem>
     </div>
+  );
+}
+
+function MenuItem(
+  props: React.PropsWithChildren<{ to: string; icon: string }>
+) {
+  const { to, icon, children } = props;
+  return (
+    <Link to={to} className={styles.item}>
+      <i className={`${styles.icon} fa fa-${icon}`} />
+      <div className={styles.label}>{children}</div>
+    </Link>
   );
 }
